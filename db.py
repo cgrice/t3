c0e05264249d89390a56173e513a5673d84f9422
chris <chris@chrisgrice.com>
2009-03-16 18:16:32 +0000

    Added punch-in, punch-out, list, and time

--- db.py
+++ db.py
@@ -22,11 +22,14 @@ class t3DB:
     def currentTicket(self):
         self.cursor.execute('''SELECT * FROM updates LIMIT 1 OFFSET (SELECT COUNT(update_id) FROM updates)-1''')
         result = self.cursor.fetchall()
-        return { 'update_id':result[0][0],
-                 'timestamp':result[0][1],
-                 'ticket':result[0][2],
-                 'punched_in':result[0][3]
-               }
+        if(len(result) > 0):
+            return { 'update_id':result[0][0],
+                     'timestamp':result[0][1],
+                     'ticket':result[0][2],
+                     'punched_in':result[0][3]
+                   }
+        else:
+            return False
 
     def update(self, ticket, punch):
         self.cursor.execute('''INSERT INTO updates(timestamp, ticket_number, punched_in)
@@ -40,11 +43,21 @@ class t3DB:
         timestamp = float(results[0][0])
         counting = False
         for result in results:
-            if(result[1]):
-                counting = True
-            else:
-                counting = False
-            if(counting):
-                time = time + (float(result[0]) - timestamp)
+            if(result[1] != 1):
+                if(counting):
+                    time = time + (float(result[0]) - timestamp)
                 timestamp = float(result[0])
-        return time        
+                counting = False
+            elif(result[1] == 1):
+                timestamp = float(result[0])
+                counting = True
+        return time   
+
+    def getTimeList(self):
+        self.cursor.execute('''SELECT DISTINCT ticket_number FROM updates''')
+        tickets = self.cursor.fetchall()
+        tlist = []
+        for ticket in tickets:
+            tlist.append( (ticket[0], self.timeForTicket(ticket[0])) )
+        return tlist
+
--- t3.py
+++ t3.py
@@ -1,45 +1,76 @@
+#!/usr/bin/python
+
 import sys
 from os import getcwd
+from db import t3DB
 
 
 class t3:
 
     def __init__(self):
         self.cwd = getcwd()
-        print self.cwd
-        #self.db = t3DB()
-        #self.current_ticket = db.CurrentTicket()
+        self.db = t3DB('./t3.db')
+        self.current_ticket = self.db.currentTicket()
         #self.status = db.Status()
     def main(self, args):
         for arg in args:
             # checkout - take ownership of a ticket, ready to punch in / punch out
             if arg == 'co' or arg == 'checkout':
-                self.checkout(args[3])
+                self.checkout(args[1])
                 break
             # punch in - tell t3 that you are working on a specific ticket
             #            $ t3 pi will punch in to your last used ticket
             #            $ t3 pi 14 will punch in to ticket 14
             if arg == 'pi' or arg == 'punch-in':
-                punchout()
                 if(len(args) > 1):
                     self.checkout(args[1])
                     self.punchin(args[1])
                 else:
-                    self.punchin(current_ticket)
+                    self.punchin(self.current_ticket)
                 break
             # punch out - stop working on your current ticket.
             if arg == 'po' or arg == 'punch-out':
                 self.punchout()
                 break
+            if arg == 'time':
+                self.getTime(args[1])
+                break
+            if arg == 'list' or arg == 'ls':
+                self.getList()
+                break
 
     def checkout(self, ticket):
+        if(self.current_ticket):
+            if(self.current_ticket['ticket'] != ticket):
+                self.punchout()
+        self.current_ticket['ticket'] = ticket
         print 'Now working on ticket ' + ticket
 
     def punchin(self, ticket):
+        self.db.update(ticket, 1)
         print 'Punching in - ticket #' + ticket
 
     def punchout(self):
-        print 'Punched out'
+        if(self.current_ticket and self.current_ticket['punched_in'] == 1):
+            self.db.update(self.current_ticket['ticket'], 0)
+            print 'Punched out of ticket #' + self.current_ticket['ticket']
+        else:
+            print 'Not punched in to any tickets'
+
+    def getTime(self, ticket):
+        time =  self.db.timeForTicket(ticket)
+        print time
+
+    def getList(self):
+        tlist = self.db.getTimeList()
+        print "Ticket\tTime Spent\t"
+        for tl in tlist:
+            if self.current_ticket['punched_in'] and self.current_ticket['ticket'] == tl[0]:
+                current = "<--\t"
+            else:
+                current = "\t"
+            print str(tl[0]) + "\t" + str(tl[1]) + current
+            
 
 if __name__ == '__main__':
     t = t3()
