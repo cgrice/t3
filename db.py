3b517d9dbeff8dae275421cb79962675adc7d359
chris <chris@chrisgrice.com>
2009-03-16 17:22:44 +0000

    added basic database timetracking

--- db.py
+++ db.py
@@ -1,18 +1,50 @@
 from pysqlite2 import dbapi2 as sqlite
+import os
+from time import time
+import datetime
 
 class t3DB:
 
     def __init__(self, path):
-        if(is_file(path)):
-            sqllite3_conn = sqlite.connect(path)
+        if(os.path.exists(path)):
+            self.conn = sqlite.connect(path)
+            self.cursor = self.conn.cursor()
         else:
-            sqllite3_conn = sqlite.connect(path)
+            self.conn = sqlite.connect(path)
+            self.cursor = self.conn.cursor()
             self._setupDB()
-        self.cursor = sqllite3_conn.cursor()
 
     def _setupDB(self):
-        # Set up tables
-        c.execute('''CREATE TABLE updates(update_id INTEGER PRIMARY KEY, timestamp INTEGER NOT NULL, 
+        self.cursor.execute('''CREATE TABLE updates(update_id INTEGER PRIMARY KEY, timestamp VARCHAR(64) NOT NULL, 
                      ticket_number NOT NULL, punched_in BOOLEAN NOT NULL)''')
+        self.conn.commit()
+        
+    def currentTicket(self):
+        self.cursor.execute('''SELECT * FROM updates LIMIT 1 OFFSET (SELECT COUNT(update_id) FROM updates)-1''')
+        result = self.cursor.fetchall()
+        return { 'update_id':result[0][0],
+                 'timestamp':result[0][1],
+                 'ticket':result[0][2],
+                 'punched_in':result[0][3]
+               }
 
+    def update(self, ticket, punch):
+        self.cursor.execute('''INSERT INTO updates(timestamp, ticket_number, punched_in)
+                               VALUES( ?, ?, ? )''', (time(), ticket, punch))
+        self.conn.commit()
 
+    def timeForTicket(self, ticket):
+        self.cursor.execute('''SELECT timestamp, punched_in FROM updates WHERE ticket_number = ?''', (ticket,))
+        results = self.cursor.fetchall()
+        time = 0
+        timestamp = float(results[0][0])
+        counting = False
+        for result in results:
+            if(result[1]):
+                counting = True
+            else:
+                counting = False
+            if(counting):
+                time = time + (float(result[0]) - timestamp)
+                timestamp = float(result[0])
+        return time        
